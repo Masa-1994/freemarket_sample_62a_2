87d8109d563d17752a6c0a484dad0f24870e6b34
DAISUKE007 <da1suke.haseee@gmail.com>
2019-11-20 14:15:11 +0900

    h-seed　ユーザー情報登録

---
+++ db/fixtures/development/user.rb
@@ -0,0 +1,13 @@
+User.seed do |s|
+  s.id                      = 1
+  s.nickname                = "アップル"
+  s.email                   = "aaa@aaaa"
+  s.encrypted_password      = "11111111"
+  s.family_name             = "苗字"
+  s.first_name              = "名前"
+  s.family_name_kana        = "ミョウジ"
+  s.first_name_kana         = "ナマエ"
+  s.birthday_year           = "2000-11-11"
+  s.phone_number            = 123456789
+  s.introduce               = "紹介文サンプルサンプルサンプルサンプルサンプル"
+end
