c8943140157e5cef54399f2142a89e10b0918015
kkminaaa <kmkmber@icloud.com>
2019-11-21 14:02:35 +0900

    o-変更

--- db/fixtures/development/image.rb
+++ db/fixtures/development/image.rb
@@ -3,3 +3,10 @@ Image.seed do |s|
   s.product_id = 1
   s.image = "http://sozaizchi.com/sozai/illust/kodomofuku/images2/03.gif"
 end
+
+Image.seed do |s|
+  s.id = 2
+  s.product_id = 2
+  s.image = "https://1.bp.blogspot.com/-NtTQSOUkR9o/XVKfuVfLHcI/AAAAAAABUEA/1JsyRnHO9-8SxPb_OYm-HKAQ10dV2C45QCLcBGAs/s400/christmas_black_santa.png"
+end
+
