7a2880ba4f2f5e96edc94fb2165ea45a2aee882a
ndfkf9 <sasasirokuro@gmail.com>
2019-11-23 18:41:18 +0900

    k-価格の表示

---
+++ app/assets/javascripts/product-price.js
@@ -0,0 +1,13 @@
+// 入力された価格に応じて販売手数料と販売利益の計算
+$(document).on('turbolinks:load',function() {
+  $(".o-priceselect").on("keyup", function() {
+    var input = ($(".o-priceselect").val());
+    var calculation = input * 0.1;
+    var commission = Math.ceil(calculation);
+    var profits = input - commission;
+
+
+    $('.o-fee__right').text("￥" + commission);
+    $('.o-profit__right').text("￥" + profits);
+  });
+});
