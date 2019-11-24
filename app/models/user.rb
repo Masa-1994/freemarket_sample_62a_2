ae7c19522ec00e6f769f9e32b3ea9416a57f7236
DAISUKE007 <da1suke.haseee@gmail.com>
2019-11-24 16:58:27 +0900

    長谷川　ユーザー新規登録ページ実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,21 +2,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
 
+          VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates               :nickname,           length: {in: 1..15}, presence: true
-  validates               :email,              length: {maximum: 50}, presence: true, format: {with: /\A\S+@\S+\.\S+\z/}
-  validates               :encrypted_password, length: {in: 6..15}, presence: true
-  validates               :family_name,        presence: true
-  validates               :first_name,         presence: true
-  validates               :family_name_kana,   presence: true
-  validates               :first_name_kana,    presence: true
-  validates               :birthday,           presence: true
-  validates               :phone_number,       length: {in: 10..11}, presence: true
-
-
-
+          # step1入力項目
+          validates :nickname,                presence: true, length: {maximum: 20}, on: :validates_step1
+          validates :email,                   presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }, on: :validates_step1
+          validates :password,                presence: true, length: {minimum: 6, maximum: 128}, on: :validates_step1
+          validates :password_confirmation,   presence: true, length: {minimum: 6, maximum: 128}, on: :validates_step1
+          validates :family_name,             presence: true, on: :validates_step1
+          validates :first_name,              presence: true, on: :validates_step1
+          validates :family_name_kana,        presence: true, on: :validates_step1
+          validates :first_name_kana,         presence: true, on: :validates_step1
+          validates :birthday_year,           presence: true, on: :validates_step1
+          validates :phone_number,            presence: true, on: :validates_step2
   # associate
   has_one                       :address,     inverse_of: :user
   accepts_nested_attributes_for :address
