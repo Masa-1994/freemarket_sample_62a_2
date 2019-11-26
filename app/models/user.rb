5e6daf49400badb5c2d37783992a04940db5f68b
DAISUKE007 <da1suke.haseee@gmail.com>
2019-11-26 20:44:38 +0900

    h-修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,16 @@ class User < ApplicationRecord
           VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
           # step1入力項目
-          validates :nickname,                presence: true, length: {maximum: 20}, on: :validates_step1
-          validates :email,                   presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }, on: :validates_step1
-          validates :password,                presence: true, length: {minimum: 6, maximum: 128}, on: :validates_step1
-          validates :password_confirmation,   presence: true, length: {minimum: 6, maximum: 128}, on: :validates_step1
-          validates :family_name,             presence: true, on: :validates_step1
-          validates :first_name,              presence: true, on: :validates_step1
-          validates :family_name_kana,        presence: true, on: :validates_step1
-          validates :first_name_kana,         presence: true, on: :validates_step1
-          validates :birthday_year,           presence: true, on: :validates_step1
-          validates :phone_number,            presence: true, on: :validates_step2
+          validates :nickname,                presence: true, length: {maximum: 20}
+          validates :email,                   presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+          validates :password,                presence: true, length: {minimum: 6, maximum: 128}
+          validates :password_confirmation,   presence: true, length: {minimum: 6, maximum: 128}
+          validates :family_name,             presence: true
+          validates :first_name,              presence: true
+          validates :family_name_kana,        presence: true
+          validates :first_name_kana,         presence: true
+          validates :birthday_year,           presence: true
+          validates :phone_number,            presence: true
   # associate
   has_one                       :address,         inverse_of: :user
   accepts_nested_attributes_for :address
