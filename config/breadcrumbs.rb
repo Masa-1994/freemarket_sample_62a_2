b4a750fbd002f0fe1860b52d49357b15a7328881
Masa-1994 <kyani_m1994@icloud.com>
2019-11-27 17:55:03 +0900

    m-パンくずリスト

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,34 +1,33 @@
-# ルート
 crumb :root do
   link "メルカリ", root_path
 end
 
-# マイページ
-crumb :user do
+crumb :mypage do
   link "マイページ", user_path
   parent :root
 end
 
-# プロフィール
 crumb :profile do
   link "プロフィール", profile_user_path
-  parent :user
+  parent :mypage
 end
 
-# 支払い方法
 crumb :payment do
   link "支払い方法", payment_user_path
-  parent :user
+  parent :mypage
 end
 
-# 本人情報の登録
+crumb :credit do
+  link "クレジットカード情報入力", credit_user_path
+  parent :mypage
+end
+
 crumb :identification do
-  link "本人情報の登録",identification_user_path
-  parent :user
+  link "本人情報の登録", identification_user_path
+  parent :mypage
 end
 
-# ログアウト
 crumb :logout do
-  link "ログアウト",logout_user_path
-  parent :user
+  link "ログアウト", logout_user_path
+  parent :mypage
 end
