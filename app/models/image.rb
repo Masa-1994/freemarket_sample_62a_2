b6d94fb435515efedb5649d29c275c1fce64f6d0
ndfkf9 <sasasirokuro@gmail.com>
2019-11-30 14:56:19 +0900

    k-編集画面、画像の表示

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ApplicationRecord
 
-  validates      :family_name,             presence: true
+  # validates      :family_name,             presence: true
   
   belongs_to     :product,   optional: true
   mount_uploader :image, ImageUploader
