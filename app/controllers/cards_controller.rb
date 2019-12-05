9ddfd6d3b3b3c4292e9a5144b3d396f73acab874
DAISUKE007 <da1suke.haseee@gmail.com>
2019-12-05 13:17:30 +0900

    長谷川ーsignup-controllerーリファクタリング

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,16 +3,14 @@ class CardsController < ApplicationController
   require "payjp"
   before_action :set_card
 
-
-  def new # カードの登録画面。送信ボタンを押すとcreateアクションへ。
+  # カードの登録画面。送信ボタンを押すとcreateアクションへ ----------------
+  def new
     creditcard = CreditCard.where(user_id: current_user.id).first
     redirect_to "/cards/new" if creditcard.present?
   end
 
-  # indexアクションはここでは省略
 
-#PayjpとCardのデータベースを作成----------------------------------------------------------------------------------------------
-
+  # PayjpとCardのデータベースを作成 --------------------------------
   def create
     Payjp.api_key = 'sk_test_96c344952e792691d9fc840e'    #Pay.jpの秘密鍵
 
@@ -21,9 +19,9 @@ class CardsController < ApplicationController
     else
       # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録します。
       customer = Payjp::Customer.create(
-        description: current_user.family_name + current_user.first_name,  #PAY.JPの顧客情報に表示する名前です。
-        email: current_user.email,                                        #PAY.JPの顧客情報に表示するemailです。
-        card: params['payjp-token'], # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐付けて永久保存します。
+        description: current_user.family_name + current_user.first_name,  #PAY.JPの顧客情報に表示する名前
+        email: current_user.email,                                        #PAY.JPの顧客情報に表示するemail
+        card: params['payjp-token'],                                      # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐付けて永久保存
         metadata: {user_id: current_user.id}                              # 無くてもOK。
       )
       @creditcard = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
@@ -37,6 +35,8 @@ class CardsController < ApplicationController
 
   private
 
+  # ログインユーザーのクレジット情報
+  #（顧客id と カードID)を取得 --------------------------------------
   def set_card
     @creditcard = CreditCard.where(user_id: current_user.id).first if CreditCard.where(user_id: current_user.id).present?
   end
--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -2,35 +2,35 @@ class SignupController < ApplicationController
 
   before_action :validates_step1, only: :step2   # step1のバリデーション
   before_action :validates_step2, only: :step3   # step2のバリデーション
-  before_action :validates_step3, only: :create  # step2のバリデーション
+  before_action :validates_step3, only: :create  # step3のバリデーション
 
-  # 新規会員登録-email-FaceBook-Google-
+  # 新規会員登録-email-FaceBook-Google ------------------------------
   def index
   end
 
-  # 会員情報入力
+  # 会員情報入力 ----------------------------------------------------
   def step1
-    @user = User.new # 新規インスタンス作成
+    @user = User.new
   end
 
-  # 電話番号の確認
+  # 電話番号の確認 --------------------------------------------------
   def step2
-    @user = User.new # 新規インスタンス作成
+    @user = User.new
   end
 
-  # 発送元・お届け先住所入力
+  # 発送元・お届け先住所入力 -------------------------------------------
   def step3
-    @user = User.new # 新規インスタンス作成
+    @user = User.new
     @user.build_address
     @address = Address.new
   end
 
-  # 登録完了
+  # 登録完了 -------------------------------------------------------
   def done
   end
 
-# 会員情報入力(STEP1)--------------------------------------------------------------------------------------------------
-
+# 会員情報入力(STEP1)
+# sessionに会員情報を一時保存 ----------------------------------------
   def validates_step1
     session[:nickname] = user_params[:nickname]
     session[:email] = user_params[:email]
@@ -43,7 +43,7 @@ class SignupController < ApplicationController
     session[:birthday_year] = user_params["birthday_year(1i)"] + "-" + user_params["birthday_year(2i)"] + "-" + user_params["birthday_year(3i)"]
     
     @user = User.new(
-      nickname: session[:nickname], 
+      nickname: session[:nickname],
       email: session[:email],
       password: session[:password],
       password_confirmation: session[:password_confirmation],
@@ -54,16 +54,16 @@ class SignupController < ApplicationController
       birthday_year: session[:birthday_year],
       phone_number: '09012345678'
     )
-    render "/signup/step1" unless @user.valid? && verify_recaptcha(model: @user)   #sessionに対してのバリデーション
+    render "/signup/step1" unless @user.valid? && verify_recaptcha(model: @user)   #session && reCAPTCHA 対してのバリデーション
   end
 
-# 電話番号の確認(STEP2)-----------------------------------------------------------------------------------------------
-
+# 電話番号の確認(STEP2)
+# sessionに電話番号を一時保存 ----------------------------------------
   def validates_step2
     session[:phone_number] = user_params[:phone_number]
     
     @user = User.new(
-      nickname: session[:nickname], 
+      nickname: session[:nickname],
       email: session[:email],
       password: session[:password],
       password_confirmation: session[:password_confirmation],
@@ -77,8 +77,8 @@ class SignupController < ApplicationController
     render "/signup/step2" unless @user.valid?    #sessionに対してのバリデーション
   end
 
-# 発送元・お届け先住所入力(STEP3)----------------------------------------------------------------------------------------
-
+# 発送元・お届け先住所入力(STEP3)
+# sessionに発送元・お届け先住所を一時保存 -------------------------------
   def validates_step3
     session[:user_id] = user_params[:address_attributes][:id]
     session[:address_family_name] = user_params[:address_attributes][:family_name]
@@ -89,6 +89,9 @@ class SignupController < ApplicationController
     session[:address_prefecture_id] = user_params[:address_attributes][:prefecture_id]
     session[:address_municipalities] = user_params[:address_attributes][:municipalities]
     session[:address_house_number] = user_params[:address_attributes][:house_number]
+    session[:building_name] = user_params[:address_attributes][:building_name]
+    session[:phone_number] = user_params[:address_attributes][:phone_number]
+
 
     @user = User.new(
       email: 'www@gmail.com',
@@ -103,48 +106,51 @@ class SignupController < ApplicationController
       prefecture_id: session[:address_prefecture_id],
       municipalities: session[:address_municipalities],
       house_number: session[:address_house_number],
+      building_name: session[:building_name],
+      phone_number: session[:phone_number]
     )
-    render "/signup/step3" unless @address.valid?    #sessionに対してのバリデーション
+    render "/signup/step3" unless @address.valid?                     # sessionに対してのバリデーション
   end
 
-# sessionのデータをデータベースに保存する----------------------------------------------------------------------------------
-
+# sessionのデータをデータベースに保存する ---------------------------------
   def create
     # usersテーブルに値を入れる
     @user = User.new(
-      nickname: session[:nickname], 
-      email: session[:email],
-      password: session[:password],
-      password_confirmation: session[:password_confirmation],
-      family_name: session[:family_name],
-      first_name: session[:first_name],
-      family_name_kana: session[:family_name_kana],
-      first_name_kana: session[:first_name_kana],
-      birthday_year: session[:birthday_year],
-      phone_number: session[:phone_number],
+      nickname: session[:nickname],                                   # ニックネーム
+      email: session[:email],                                         # メールアドレス
+      password: session[:password],                                   # パスワード
+      password_confirmation: session[:password_confirmation],         # パスワード（確認用）
+      family_name: session[:family_name],                             # 苗字（漢字）
+      first_name: session[:first_name],                               # 名前(漢字)
+      family_name_kana: session[:family_name_kana],                   # 苗字（カナ）
+      first_name_kana: session[:first_name_kana],                     # 名前（カナ）
+      birthday_year: session[:birthday_year],                         # 生年月日
+      phone_number: session[:phone_number]                            # 電話番号
     )
     # addressテーブルに値を入れる
     @user.build_address(
-      family_name: session[:address_family_name],
-      first_name: session[:address_first_name],
-      family_name_kana: session[:address_family_name_kana],
-      first_name_kana: session[:address_first_name_kana],
-      postal_code: session[:address_postal_code],
-      prefecture_id: session[:address_prefecture_id],
-      municipalities: session[:address_municipalities],
-      house_number: session[:address_house_number],
+      family_name: session[:address_family_name],                     # 苗字（漢字） アドレス用
+      first_name: session[:address_first_name],                       # 名前（漢字） アドレス用
+      family_name_kana: session[:address_family_name_kana],           # 苗字（カナ） アドレス用
+      first_name_kana: session[:address_first_name_kana],             # 名前（カナ） アドレス用
+      postal_code: session[:address_postal_code],                     # 郵便番号
+      prefecture_id: session[:address_prefecture_id],                 # 都道府県
+      municipalities: session[:address_municipalities],               # 市区町村
+      house_number: session[:address_house_number],                   # 番地
+      building_name: session[:building_name],                         # 建物名
+      phone_number: session[:phone_number]                            # 電話番号 アドレス用
     )
-    if @user.save   #データベースへの保存
+    # #データベースへの保存
+    if @user.save
       session[:id] = @user.id
-      sign_in User.find(session[:id]) unless user_signed_in? # 自動ログイン
-      redirect_to "/cards/new" #データベースに保存されれば、クレジットカード登録ページに移動する
+      sign_in User.find(session[:id]) unless user_signed_in?          # 自動ログイン
+      redirect_to "/cards/new"                                        #データベースに保存されれば、クレジットカード登録ページに移動する
     else
-      render '/signup/step3'   #データベースに保存されなければ,会員情報入力(STEP3)からやり直し
+      render '/signup/step3'                                          # データベースに保存されなければ,会員情報入力(STEP3)からやり直し
     end
   end
 
-  #新規登録ページのformデータを取得--------------------------------------------------------------------------------------
-
+  #新規登録ページのformデータを取得 --------------------------------------
   private
 
   def user_params
@@ -164,6 +170,4 @@ class SignupController < ApplicationController
       address_attributes: [:id, :family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number]
     )
   end
-
-
 end
